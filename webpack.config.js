3e6bd4bb6e495306e64934b00572ed4d7217101a
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-21 17:09:38 -0600

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    // Change mode to production after finishing development
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/i,
+                use: ['style-laoder', 'css-loader']
+            }
+        ],
+    },
+}
