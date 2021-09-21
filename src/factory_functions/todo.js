ac65d18758ac3ace309d18f7c1dcf96be9071a15
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-21 17:56:20 -0600

    Complete todo factory function

---
+++ src/factory_functions/todo.js
@@ -0,0 +1,27 @@
+// todo prototype
+const todoProto = {
+    changeTitle: function(newTitle) {
+        this.title = newTitle;
+    },
+    changeDescription: function(newDescription) {
+        this.description = newDescription;
+    },    
+    changeDueDate: function(newDueDate) {
+        this.dueDate = newDueDate;
+    },
+    changePriority: function(newPriority) {
+        this.priority = newPriority;
+    }
+}
+
+// todo factory function
+function todo(title, description, dueDate, priority) {
+    return Object.assign(Object.create(todoProto), {
+        title,
+        description,
+        dueDate,
+        priority
+    });
+}
+
+export { todo };
--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
         rules: [
             {
                 test: /\.css$/i,
-                use: ['style-laoder', 'css-loader']
+                use: ['style-loader', 'css-loader']
             }
         ],
     },
