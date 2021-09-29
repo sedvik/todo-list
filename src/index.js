7f9b4e33289e0f53af225ecab4d9812905dc53f9
Skyler Edvik <edvik.skyler@gmail.com>
2021-09-28 23:07:33 -0600

    Add localStorage functionality

--- src/index.js
+++ src/index.js
@@ -3,38 +3,8 @@ import { events } from './modules/events.js';
 import { storage } from './modules/storage.js';
 import { app } from './modules/app.js';
 
-// TEMPORARY projects array that will stand in for localStorage to rehydrate into appropriate objects with prototypes
-const projects = [
-    {
-        name: 'Food Project',
-        todos: [
-            {
-                title: 'Eat some pizza',
-                description: 'Eat some pizza with garlic sauce',
-                dueDate: '2012-10-22',
-                priority: 'high'
-            },
-            {
-                title: 'Eat some chicken wings',
-                description: 'Eat some chicken wings with ranch',
-                dueDate: '2018-03-14',
-                priority: 'mid'
-            }
-        ]
-    },
-    {
-        name: 'Guitar Project',
-        todos: [
-            {
-                title: 'Practice Stairway to Heaven',
-                description: 'Practice the chorus of Stairway to Heaven',
-                dueDate: '2021-05-18',
-                priority: 'low'
-            }
-        ]
-    }
-];
-
+// Retrieve projects from localStorage
+const projects = storage.load();
 
 // Initialize viewController, events, and storage modules
 viewController.init();
